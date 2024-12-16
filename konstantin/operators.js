ecf9d97e3d543eeaeab63a33d134663974718290
konstantin.volkov <konstantin.volkov@simbirsoft.com>
2024-12-16 16:57:09 +0100

    finished second operators lesson

--- konstantin/operators.js
+++ konstantin/operators.js
@@ -19,3 +19,31 @@ console.log(volume);
 const city = "Белград";
 const street = "Кисловодская";
 console.log(city + ", " + street + " " + 5);
+
+/*
+    Операторы присваивания
+*/
+
+let age = 18 + 5;
+
+age = age += 2; // age = age = 2
+age = age -= 5; // age = age - 2
+age = age *= 2; // age = age * 2
+age = age /= 2; // age = age / 2
+
+age++; // age = age + 1
+age--; // age = age - 1
+console.log(age);
+
+/*
+    Операторы сравнения
+*/
+
+const customerAge = 20;
+
+console.log(customerAge > age);
+console.log(customerAge >= age);
+console.log(customerAge < age);
+console.log(customerAge <= age);
+console.log(customerAge == age);
+console.log(customerAge != age);
