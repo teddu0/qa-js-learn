8bf20cf3ef260cb99c321ff656d88ad9115563e8
konstantin.volkov <konstantin.volkov@simbirsoft.com>
2024-12-16 16:30:34 +0100

    finished first arithmetic ops lesson

---
+++ konstantin/arithmetic.operations.js
@@ -0,0 +1,21 @@
+/*
+    Арифметические операции
+*/
+
+// Базовые арифметические операции
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3; // возведение в степень (2 * 2 * 2)
+
+console.log(volume);
+
+// Строки (конкатенация)
+
+const city = "Белград";
+const street = "Кисловодская";
+console.log(city + ", " + street + " " + 5);
