7c95f03e869c1ab284aff906cda9dca31ce0e184
Kartest <qa.kartest@gmail.com>
2024-12-17 23:37:55 +0100

    kartest4.2

--- roman/hello.world.js
+++ roman/hello.world.js
@@ -1,5 +1,19 @@
 
 let a = 1;
 console.log(a)
-a = 'test1';
+a = 'test';
 console.log(a)
+
+// Базовые прифметические операторы
+const widht = 10;
+const height = 5;
+const cpase = widht * height;
+console.log(cpase)
+
+
+// Строки
+
+const city = 'Moscow';
+const street = 'Lenina'
+const adres = city + street;
+console.log(adres)
