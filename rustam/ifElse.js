0e6e8cae1b8f9168977279024759104e05791722
Rustam <javastripper96@gmail.com>
2024-12-18 18:59:39 +0300

    if-else lesson

---
+++ rustam/ifElse.js
@@ -0,0 +1,10 @@
+let money = 20;
+let canBuy = money >= 50;
+
+if (money >= 50) {
+    console.log('Может купить наш продукт');
+} else if (money >= 20) {
+    console.log('Может купить наш mini продукт');
+} else {
+    console.log('Не хватает средств на балансе');
+}
