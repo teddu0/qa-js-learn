020a0d35d3892e6ba7ac3bf97ff7077f8433955c
teddu0 <teddu0@gmail.com>
2024-12-20 00:45:45 +0100

    .

--- konstantin/flow/ifElse.conditions.js
+++ konstantin/flow/ifElse.conditions.js
@@ -8,7 +8,7 @@ const lenovoBook = 50000;
 
 if (balance >= macBookCost) {
     console.log("Покупаем Мак!!!");
-} else if (balance >= lenovoBook){
+} else if (balance >= lenovoBook) {
     console.log("Покупаем Lenovo!!!");
 } else {
     console.log("Мы бомжи!!!");
