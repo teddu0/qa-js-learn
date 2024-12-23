e31aaf77957fe37eaefe325be80155d1d85a9240
Kartest <qa.kartest@gmail.com>
2024-12-23 17:08:53 +0100

    if_else

---
+++ roman/paragraf5_if_else.js
@@ -0,0 +1,14 @@
+const money  = 10;
+const canBuy = money > 50;
+if (money > 50) 
+    {
+    console.log ('Может купить продукт');
+} else if (money => 10) {
+    console.log('Только по нюхатьэ')
+
+} else {
+    console.log ('Иди отсюда бомжара');
+}
+console.log('Итог');
+
+
