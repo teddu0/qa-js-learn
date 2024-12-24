201908844ee06e20e5b8ddfe5085da3a88965013
teddu0 <teddu0@gmail.com>
2024-12-24 19:01:52 +0100

    finished swithcCase lesson

---
+++ konstantin/flow/switchCase.js
@@ -0,0 +1,35 @@
+/*
+    Switch case conditions
+*/
+
+const rolesList = ["admin", "manager", "vaiter", "security", "cook"];
+const randomIndex = Math.floor(Math.random() * rolesList.length);
+const randomRole = rolesList[randomIndex];
+console.log(randomRole);
+
+switch (randomRole) {
+  case "cook":
+  case "vaiter":
+    console.log("Доступ к кухне");
+    break;
+  case "manager":
+    console.log("Доступ к кухне и ресепшену");
+    break;
+  case "security":
+    console.log("Доступ к информации о сотрудниках");
+    break;
+  case "admin":
+    console.log("Полный доступ");
+}
+
+switch (true) {
+  case randomIndex === 1:
+  case randomIndex === 2:
+  case randomIndex === 3:
+  case randomIndex === 4:
+    console.log("Рандомное число больше != 0");
+    break;
+  default:
+    console.log(randomIndex);
+    break;
+}
