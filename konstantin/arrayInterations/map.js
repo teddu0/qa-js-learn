f69a2b38502b0702ba86be20d3620bc8776d1ab9
konstantin.volkov <konstantin.volkov@1win.pro>
2025-04-23 00:39:01 +0200

    map additional lesson done

--- konstantin/arrayInterations/map.js
+++ konstantin/arrayInterations/map.js
@@ -1,13 +1,26 @@
-const transactionsInUSD = [100, -80, -500, 3000, -400];
+// const transactionsInUSD = [100, -80, -500, 3000, -400];
 
-const transactionsInRUB = [];
-for (const transactioin of transactionsInUSD) {
-  transactionsInRUB.push(transactioin * 60);
-}
-// console.log(transactionsInUSD);
-// console.log(transactionsInRUB);
+// const transactionsInRUB = [];
+// for (const transactioin of transactionsInUSD) {
+//   transactionsInRUB.push(transactioin * 60);
+// }
+// // console.log(transactionsInUSD);
+// // console.log(transactionsInRUB);
 
-const newTransactionInRUB = transactionsInUSD.map(
-  (transactioin) => transactioin * 60
-);
-console.log(newTransactionInRUB);
+// const newTransactionInRUB = transactionsInUSD.map(
+//   (transactioin) => transactioin * 60
+// );
+// console.log(newTransactionInRUB);
+
+const users = [
+  { firstName: "Andre", age: 23 },
+  { firstName: "Makare", age: 43 },
+  { firstName: "Monika", age: 19 },
+  { firstName: "John", age: 99 },
+  { firstName: "Max", age: 43 },
+  { firstName: "Tax", age: 3 },
+  { firstName: "Alex", age: 6 },
+  { firstName: "Roman", age: 5 },
+];
+
+const userNames = users.map((name) => console.log(name.firstName));
