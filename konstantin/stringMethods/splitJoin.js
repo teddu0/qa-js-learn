76834505e911d2abed57e9939269f70d75bfdae2
konstantin.volkov <konstantin.volkov@1win.pro>
2025-04-27 22:29:50 +0300

    split join done

---
+++ konstantin/stringMethods/splitJoin.js
@@ -0,0 +1,9 @@
+const userName = "Pupkin Igor Ivanovich";
+
+const [firstName, familyName, lastName] = userName.split(" ");
+console.log(firstName);
+console.log(familyName);
+console.log(lastName);
+
+const array = ["You", "know", "JS"];
+console.log(array.join(" "));
